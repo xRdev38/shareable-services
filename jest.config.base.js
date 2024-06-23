676d8a169816b8206127624b9c352386fe24ccd9
developer rudy <developer.rudy@gmail.com>
2024-06-23 21:17:52 +0200

    initial commit

---
+++ jest.config.base.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = {
+    'roots': [
+        '<rootDir>/packages',
+    ],
+    'transform': {
+        '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    },
+    'moduleFileExtensions': [
+        'ts',
+        'js',
+        'jsx',
+        'json',
+        'node',
+    ],
+    'collectCoverage': false,
+    'verbose': true,
+}
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const base = require('./jest.config.base.js')
+
+module.exports = {
+    ...base,
+    'projects': [
+        '<rootDir>/packages/*/jest.config.js',
+    ],
+    'coverageDirectory': '<rootDir>/coverage/',
+}
