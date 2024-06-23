7f9b42ecdce48016fcb2fa566a5ae7b014a026a3
developer rudy <developer.rudy@gmail.com>
2024-06-23 21:55:28 +0200

    feat(browserslist): add browserslist configuration

---
+++ packages/browserslist-config/README.md
@@ -0,0 +1,27 @@
+# Browserslist Config
+
+My personal shareable browserslist configuration.
+
+## Install
+
+```bash
+yarn add -D @waldronmatt/browserslist-config
+```
+
+## Usage
+
+**`.browserslistrc`**
+
+```bash
+extends @waldronmatt/browserslist-config
+```
+
+## Under The Hood
+
+### `index.json`
+
+- `browserslist`
+
+## License
+
+MIT
---
+++ packages/browserslist-config/scripts/postinstall.js
@@ -0,0 +1,9 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
+const fs = require('node:fs');
+const path = require('node:path');
+const filePath = path.join(process.env.INIT_CWD, '.browserslistrc');
+const fileConfigObject = 'extends @xrdev_38/browserslist-config';
+
+if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, fileConfigObject);
+}
