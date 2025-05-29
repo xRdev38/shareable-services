e74cf635d72f9906de739a2505513053456e1ff2
rudy <rudy.nosile@educlever.com>
2025-05-29 14:26:09 +0200

    feat: add semantic-release config

---
+++ packages/semantic-release-config/release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: ["main", "develop"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github"
+    ]
+};
